a4dfa32090cf4a7a6c68fcbb3208752894fa766a
Neel4349s <87425660+Neel4349s@users.noreply.github.com>
2021-07-20 09:42:12 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,148 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":["81a4f500-ad11-4009-8f0e-175b165af74b","11d5d9f0-6932-45de-ba25-6f4ac37fe9a9","7d58941f-a7d8-42da-85c6-3efa6131461a","42b7aa29-c5f8-44fe-9d49-8540deca9c13","e92d7a76-cb9d-426f-90f2-1f5356f539aa"],"propsByKey":{"81a4f500-ad11-4009-8f0e-175b165af74b":{"name":"ball","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/81a4f500-ad11-4009-8f0e-175b165af74b.png","frameSize":{"x":20,"y":20},"frameCount":1,"looping":true,"frameDelay":4,"version":"h7W6uzXeXhtrw7otdMOiRPOsUYNvI56i","loadedFromSource":true,"saved":true,"sourceSize":{"x":20,"y":20},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/81a4f500-ad11-4009-8f0e-175b165af74b.png"},"11d5d9f0-6932-45de-ba25-6f4ac37fe9a9":{"name":"robot","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/11d5d9f0-6932-45de-ba25-6f4ac37fe9a9.png","frameSize":{"x":77,"y":69},"frameCount":1,"looping":true,"frameDelay":4,"version":"AOO.XvmVRHyPnocaDMRjlnPxRqry85fZ","loadedFromSource":true,"saved":true,"sourceSize":{"x":77,"y":69},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/11d5d9f0-6932-45de-ba25-6f4ac37fe9a9.png"},"7d58941f-a7d8-42da-85c6-3efa6131461a":{"name":"player","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/7d58941f-a7d8-42da-85c6-3efa6131461a.png","frameSize":{"x":60,"y":91},"frameCount":1,"looping":true,"frameDelay":4,"version":"F1_V.wx0xI79.JfyOlRUl_GhQv0sXtyq","loadedFromSource":true,"saved":true,"sourceSize":{"x":60,"y":91},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/7d58941f-a7d8-42da-85c6-3efa6131461a.png"},"42b7aa29-c5f8-44fe-9d49-8540deca9c13":{"name":"player_kick","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/42b7aa29-c5f8-44fe-9d49-8540deca9c13.png","frameSize":{"x":77,"y":77},"frameCount":1,"looping":true,"frameDelay":4,"version":"3CI_S5bTkmiUATp5ovmBiFWaj_FSWGPq","loadedFromSource":true,"saved":true,"sourceSize":{"x":77,"y":77},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/42b7aa29-c5f8-44fe-9d49-8540deca9c13.png"},"e92d7a76-cb9d-426f-90f2-1f5356f539aa":{"name":"player_fall","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/e92d7a76-cb9d-426f-90f2-1f5356f539aa.png","frameSize":{"x":92,"y":51},"frameCount":1,"looping":true,"frameDelay":4,"version":"zHbvmfeB.xJwzmJ0WXlDpLE5urAMahoh","loadedFromSource":true,"saved":true,"sourceSize":{"x":92,"y":51},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/e92d7a76-cb9d-426f-90f2-1f5356f539aa.png"}}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+
+
+var playerPaddle= createSprite(370,200,10,100);
+playerPaddle.shapeColor="blue";
+playerPaddle.setAnimation("player");    
+var computerPaddle= createSprite(35,200,10,100);
+computerPaddle.shapeColor="red";
+computerPaddle.setAnimation("robot");    
+var ball= createSprite(200,200,10,10);
+ball.shapeColor="yellow";
+stopSound("assets/default.mp3");
+ball.setAnimation("ball");
+
+function draw() {
+  background("white");
+  if(ball.isTouching(playerPaddle) || ball.isTouching(computerPaddle))
+  {
+    playSound("assets/hit.mp3");
+  }
+  if(keyWentDown("k"))
+  {
+    playerPaddle.setAnimation("player_kick");
+  }
+  if(keyWentUp("k"))
+  {
+    playerPaddle.setAnimation("player");
+  }
+  
+  if (keyDown("up")) {
+    playerPaddle.y=playerPaddle.y-10;
+  }
+  
+  if (keyDown("down")) {
+    playerPaddle.y=playerPaddle.y+10;
+  }
+  
+  if(keyDown("space"))
+  {
+     ball.velocityX=2;
+     ball.velocityY=3;
+     playerPaddle.setAnimation("player");
+  }
+  
+  computerPaddle.y=ball.y;
+
+  drawnet();
+  
+  if(ball.x<0 || ball.x>400)
+  {
+    playSound("assets/score.mp3");
+    playerPaddle.setAnimation("player_fall");
+    text("Game Over",200,200);
+    resetball();
+     
+  }
+    
+  createEdgeSprites();
+  
+  if(ball.isTouching(topEdge) || ball.isTouching(bottomEdge))
+  {
+    playSound("assets/wall_hit.mp3");
+  }
+  
+  ball.bounceOff(topEdge);
+  ball.bounceOff(bottomEdge);
+  ball.bounceOff(computerPaddle);
+  ball.bounceOff(playerPaddle);
+  
+  playerPaddle.bounceOff(topEdge);
+  playerPaddle.bounceOff(bottomEdge);
+  
+  computerPaddle.bounceOff(topEdge);
+  computerPaddle.bounceOff(bottomEdge);
+  drawSprites();
+}
+
+
+
+function drawnet()
+{  
+  for(var num=0;num<400;num=num+20)
+  {
+    line(200,num,200,num+10);
+  }
+}
+
+function resetball()
+{
+  ball.x=200;
+  ball.y=200;
+  ball.velocityY=0;
+  ball.velocityX=0;
+}
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
